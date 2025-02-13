e22060bf5f82c50d37a1229f8dfcd9b004f19f77
Valeria N <riranstars@gmail.com>
2025-02-13 13:32:45 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,14 +13,39 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(
-          Icons.menu, 
-          color: Colors.grey[800]),
+        leading: Padding(
+          padding: const EdgeInsets.only(left:24.0),
+          child: Icon(
+            Icons.menu, 
+            color: Colors.grey[800]),
+        ),
         actions: [
-          Icon(
-            Icons.person)],
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(
+              Icons.person),
+          )],
       ),
+      body: Column(
+        children:[
+        //1) Texto principal 
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal:24.0),
+          child: Row(
+            children: [
+              Text("I want to ", style:TextStyle(fontSize: 32)),
+              Text("Eat", style:TextStyle(fontSize: 32, fontWeight: FontWeight.bold, decoration: TextDecoration.underline))
+            ],
+          ),
+        )
+        //2) Pestañas  (TabBar) 
+        //3) Contenido de pestañas (TabBarView) 4) Carrito (Cart)
 
+        ],
+        //4 elems de un widget: 
+
+
+      )
     );
   }
 }
